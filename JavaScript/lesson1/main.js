78a201a1b868f8091e375b84484c219f862f899b
yuriykuibida <george94urich@gmail.com>
2022-07-21 21:44:24 +0300

    HW1

---
+++ JavaScript/lesson1/main.js
@@ -0,0 +1,92 @@
+let w1 = 'hello';
+console.log(w1);
+alert(w1);
+document.write(w1);
+
+let w2 = 'owu';
+console.log(w2);
+alert(w2);
+document.write(w2);
+
+let w3 = 'com';
+console.log(w3);
+alert(w3);
+document.write(w3);
+
+let w4 = 'ua';
+console.log(w4);
+alert(w4);
+document.write(w4);
+
+let n1 = 1;
+console.log(n1);
+alert(n1);
+document.write(n1);
+
+let n2 = 10;
+console.log(n2);
+alert(n2);
+document.write(n2);
+
+let n3 = -999;
+console.log(n3);
+alert(n3);
+document.write(n3);
+
+let n4 = 123;
+console.log(n4);
+alert(n4);
+document.write(n4);
+
+let n5 = 3.14;
+console.log(n5);
+alert(n5);
+document.write(n5);
+
+let n6 = 2.7;
+console.log(n6);
+alert(n6);
+document.write(n6);
+
+let n7 = 16;
+console.log(n7);
+alert(n7);
+document.write(n7);
+
+let b1 = true;
+console.log(b1);
+alert(b1);
+document.write(b1);
+
+let b2 = false;
+console.log(b2);
+alert(b2);
+document.write(b2);
+
+let firstName = 'Куйбіда ';
+let middleName = 'Юрій ';
+let lastName = 'Володимирович';
+let person = firstName + middleName + lastName;
+console.log(person);
+
+let a = 100;
+console.log(typeof  a);
+
+let b = '100';
+console.log(typeof  b);
+
+let c = true;
+console.log(typeof  c);
+
+let name = prompt("Ім'я");
+console.log(name);
+
+let name2 = prompt("По батькові");
+console.log(name2);
+
+let year = prompt("Рік");
+console.log(year);
+
+
+
+
