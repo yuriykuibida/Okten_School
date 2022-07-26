9c38ced13e155a69fcb812099375708bf4f15b2f
yuriykuibida <george94urich@gmail.com>
2022-07-26 23:58:49 +0300

    HW2

---
+++ JavaScript/lesson2/main.js
@@ -0,0 +1,91 @@
+// - Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
+// let rockbands = ['Kiss', 'AC/DC', 'Motley Crue', 'Twisted Sister', 'Alice Cooper', 'Queen', 'Poison', 'Cinderella', 'Dokken', 'Ratt'];
+// console.log(rockbands[0]);
+// console.log(rockbands[1]);
+// console.log(rockbands[2]);
+// console.log(rockbands[3]);
+// console.log(rockbands[4]);
+// console.log(rockbands[5]);
+// console.log(rockbands[6]);
+// console.log(rockbands[7]);
+// console.log(rockbands[8]);
+// console.log(rockbands[9]);
+
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
+// let TheTokyoZodiacMurders = {
+//     title: 'The Tokyo Zodiac Murders',
+//     pageCount: 251,
+//     genre: 'Murder mystery'
+// };
+//
+// let SKing = {
+//     title: '11.22.63',
+//     pageCount: 849,
+//     genre: 'Science fiction'
+// };
+//
+// let Firm = {
+//     title: 'The Firm',
+//     pageCount: 432,
+//     genre: 'Legal thriller'
+// };
+
+
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors. Поле "автори" - масив. Кожен автор має поля name,age
+// let TheTokyoZodiacMurders = {
+//     title: 'The Tokyo Zodiac Murders',
+//     pageCount: 251,
+//     genre: 'Murder mystery',
+//     authors: [
+//         {name:'Soji Shimada', age: 73},
+//         {name:'James Hatfield', age: 58},
+//         {name:'Lars Ulrich', age: 59}
+//     ]
+// };
+//
+// let SKing = {
+//     title: '11.22.63',
+//     pageCount: 849,
+//     genre: 'Science fiction',
+//     authors: [
+//         {name:'Steohen King', age: 74},
+//         {name:'Splinter', age: 42}
+//     ]
+// };
+//
+// let Firm = {
+//     title: 'The Firm',
+//     pageCount: 432,
+//     genre: 'Legal thriller',
+//     authors: [
+//         {name:'John Grisham', age: 67},
+//         {name:'Shredder', age: 38}
+//     ]
+// };
+
+
+// - Створити масив з 10 об'єктами які описують сутніть "користувач". Поля: name, username,password. Вивести в консоль пароль кожного користувача
+
+// let users = [
+//     {name:'Ivan', username:'Vania1999', password: 'qwerty'},
+//     {name:'Pavlo', username:'Pashka', password: '123'},
+//     {name:'Igor', username:'Igor007', password: '4572'},
+//     {name:'Olha', username:'OlhaOlha', password: 'asdasdas'},
+//     {name:'Kateryna', username:'Kate', password: 'Kate228'},
+//     {name:'Vasyl', username:'Vasia1', password: 'xzcvxcbv'},
+//     {name:'Maria', username:'MaryMary', password: 'hhhhhhh'},
+//     {name:'Iryna', username:'Ira', password: 'tyuiui'},
+//     {name:'Orest', username:'Orest', password: 'nmvhghbf'},
+//     {name:'Marta', username:'Marta000', password: 'qwerty123321'}
+// ];
+//
+// console.log(users [0] ['password']);
+// console.log(users [1] ['password']);
+// console.log(users [2] ['password']);
+// console.log(users [3] ['password']);
+// console.log(users [4] ['password']);
+// console.log(users [5] ['password']);
+// console.log(users [6] ['password']);
+// console.log(users [7] ['password']);
+// console.log(users [8] ['password']);
+// console.log(users [9] ['password']);
